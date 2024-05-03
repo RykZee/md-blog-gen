43a3f97ae735d01572f89f20491e380e940437f5
Sebastian Rykowski Zeerak <sebastian.zeerak@gmail.com>
2024-05-03 10:30:54 +0200

    removed experimental typescript file in scripts folder

--- scripts/generate-html.ts
+++
@@ -1,7 +0,0 @@
-import path from 'path';
-// fs = require("fs");
-// matter = require("gray-matter");
-// import remark from "remark";
-// html = require("remark-html");
-
-console.log("Success if you reached this point");
