fdc59dd8cd3b24dfe7249af7f9d6762651c04afe
Sebastian Rykowski Zeerak <sebastian.zeerak@gmail.com>
2024-05-07 14:13:43 +0200

    Clean home page instead of next.js template

--- app/ui/home/navbar.tsx
+++ app/ui/home/navbar.tsx
@@ -14,14 +14,14 @@ const links = [
 export default function Navbar() {
   const pathname = usePathname();
   return (
-    <div className="flex justify-normal items-normal w-full h-20 px-4 fixed nav">
+    <div className="fixed left-0 top-0 flex w-full border-b bg-gradient-to-b from-zinc-200 pb-4 pt-4 dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:p-4 lg:dark:bg-zinc-800/30">
       {links.map((link) => {
         return (
           <Link
             key={link.name}
             href={link.href}
             className={clsx(
-              "flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3",
+              "flex h-[40px] grow justify-center rounded-md bg-gray-50 text-base font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3",
               {
                 "bg-sky-100 text-blue-600": pathname === link.href,
               }
